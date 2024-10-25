2f695b886db2e7fce25d549bd196d892ce68200f
jgf-osu <81531640+jgf-osu@users.noreply.github.com>
2024-10-25 11:27:14 -0400

    refactor "creating_bricks"
    Extract "PlaceBrick" and "PaintBrick" methods.

--- Assets/scripts/creating_bricks.cs
+++ Assets/scripts/creating_bricks.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class creating_bricks : MonoBehaviour
@@ -16,27 +14,32 @@ public class creating_bricks : MonoBehaviour
     public int col_len, row_len;
     public float x_space, y_space;
     public float x_start, y_start;
-    public GameObject brick;
+    public GameObject Brick;
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 start_block_placement = new Vector3 (x_start, y_start);
-        for(int i = 0; i < col_len * row_len; i++){
-            Vector3 block_spacing = new Vector3 (x_space * (i % col_len),
+        Vector3 start_block_placement = new Vector3(x_start, y_start);
+        for (int i = 0; i < col_len * row_len; i++)
+        {
+            Vector3 block_spacing = new Vector3(x_space * (i % col_len),
             y_start + (y_space * (i / col_len)));
 
-            Vector3 next_blocK_placement = start_block_placement + block_spacing;
-
-            GameObject new_brick = Instantiate(brick, next_blocK_placement, Quaternion.identity);
-            Renderer renderer = new_brick.GetComponent<Renderer>();
-            renderer.material.color = color_arr[(i/col_len) % color_arr.Length];
+            Vector3 next_block_placement = start_block_placement + block_spacing;
+            PlaceBrick(next_block_placement, color_arr[(i / col_len) % color_arr.Length]);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaceBrick(Vector3 position, Color color)
     {
-        
+        GameObject new_brick = Instantiate(original: Brick, position, Quaternion.identity);
+        PaintBrick(new_brick, color);
     }
+
+    public void PaintBrick(GameObject brick, Color color)
+    {
+        Renderer renderer = brick.GetComponent<Renderer>();
+        renderer.material.color = color;
+    }
+
 }
